bc6676600c4f942c415b4bba5e75fcc32a90aefd
carowoline <caro98.jpg@gmail.com>
2023-01-12 19:52:05 -0500

    finished part 1..?

--- db/index.js
+++ db/index.js
@@ -11,18 +11,14 @@ async function getAllUsers() {
 }
 
 async function createUser({ username, password }) {
-    try {
         const result = await client.query(`
             INSERT INTO users (username, password)
             VALUES ($1, $2)
             ON CONFLICT (username) DO NOTHING
-            RETURNING id, username, password;
+            RETURNING *;
     `, [username, password]);
 
         return result;
-    } catch (error) {
-        throw error;
-    }
 }
 
 // async function createUser({ username, password }) {
--- db/seed.js
+++ db/seed.js
@@ -44,8 +44,12 @@ async function createInitialUsers() {
     console.log("Starting to create users...");
 
     const albert = await createUser({ username: 'albert', password: 'bertie99' });
+    const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
+    const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
 
     console.log(albert);
+    console.log(sandra);
+    console.log(glamgal);
 
     console.log("Finished creating users!");
   } catch (error) {
