d76fd5ba186d525c8ad17d9acc09c39f585c3bab
carowoline <caro98.jpg@gmail.com>
2023-01-12 19:46:20 -0500

    part 1

---
+++ db/index.js
@@ -0,0 +1,47 @@
+const { Client } = require('pg');
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+        `SELECT id, username 
+        FROM users;
+    `);
+
+    return rows;
+}
+
+async function createUser({ username, password }) {
+    try {
+        const result = await client.query(`
+            INSERT INTO users (username, password)
+            VALUES ($1, $2)
+            ON CONFLICT (username) DO NOTHING
+            RETURNING id, username, password;
+    `, [username, password]);
+
+        return result;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// async function createUser({ username, password }) {
+//     try {
+//         const result = await client.query(`
+//       INSERT INTO users (username, password)
+//       VALUES ($1, $2)
+//       ON CONFLICT (username) DO NOTHING
+//       RETURNING *;
+//     `, [username, password]);
+
+//         return result;
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+
+module.exports = {
+    client,
+    getAllUsers,
+    createUser,
+}
